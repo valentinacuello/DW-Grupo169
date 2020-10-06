170e7b5ff39071c4b1402672a740334ae0ed7870
valentinacuello <68921875+valentinacuello@users.noreply.github.com>
2020-10-06 17:45:42 -0300

    ...

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,25 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+
+var articleCart = [];
+var artItem = {};
+
 document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(CART_INFO_URL).then(function (resultObj){
+      if (resultObj.status === "ok"){
+        artItem = resultObj.data;
+
+          let articleImgHTML = document.getElementById("imgArt");
+          let articleNameHTML = document.getElementById("articleName");
+          let articleCostHTML = document.getElementById("articleCost");
+
+
+          articleImgHTML.innerHTML = artItem.articles[0].src;
+          articleNameHTML.innerHTML = artItem.articles[0].name;
+          articleCostHTML.innerHTML = artItem.articles[0].currency + " " + artItem.articles[0].unitCost; 
+
+      }     
+    });   
 
 });
