676d13227214ae82799ca3f065ca616d219f94ea
valentinacuello <68921875+valentinacuello@users.noreply.github.com>
2020-10-08 13:40:27 -0300

    Entrega 5

--- js/cart.js
+++ js/cart.js
@@ -2,24 +2,59 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 
-var articleCart = [];
-var artItem = {};
+var itemCarrito = {};
+
+
+function carritoDeCompras(itemCarrito){
+  let htmlContentToAppend = "";
+  
+
+  for(let i = 0; i < 2; i++){
+    htmlContentToAppend += `
+        <tr>
+          <td class="td-articulo">
+            <div class="imagen-art card" id="imgArt">
+            <img src="` + itemCarrito.articles[i].src + `" class="imagenProducto">
+            </div>
+            <div class="nombre-art">
+              <p>
+              `+ itemCarrito.articles[i].name +`
+              </p>
+            </div>
+          </td>
+
+          <td>
+          <p id="precio">
+              `+ "$ "+ itemCarrito.articles[i].unitCost + " " + itemCarrito.articles[i].currency +`
+              </p>
+          </td>
+
+          <td>
+            <div class="input-cantidad">
+              <input type="number" name="" id="" value="1">
+            </div>
+          </td>
+        </tr>
+    
+    `
+  }
+
+  document.getElementById("articulosDelCarrito").innerHTML = htmlContentToAppend;
+};
+
 
 document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(CART_INFO_URL).then(function (resultObj){
-      if (resultObj.status === "ok"){
-        artItem = resultObj.data;
+  getJSONData(CART_INFO_DOS_URL).then(function (resultObj){
+    if (resultObj.status === "ok"){
 
-          let articleImgHTML = document.getElementById("imgArt");
-          let articleNameHTML = document.getElementById("articleName");
-          let articleCostHTML = document.getElementById("articleCost");
+      itemCarrito = resultObj.data;
+      
+      carritoDeCompras(itemCarrito);
+
+    }
 
 
-          articleImgHTML.innerHTML = artItem.articles[0].src;
-          articleNameHTML.innerHTML = artItem.articles[0].name;
-          articleCostHTML.innerHTML = artItem.articles[0].currency + " " + artItem.articles[0].unitCost; 
-
-      }     
-    });   
+  });
 
 });
+
